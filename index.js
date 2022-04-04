f52bc16480b8767e76b97283aa1a936566327ef8
Divya <divyabhatt16@gmail.com>
2022-04-04 11:48:18 +0100

    Implemented images on both cards and outcome and displayed on screen

--- index.js
+++ index.js
@@ -1,46 +1,69 @@
 const getRandomMove = () => {
-    const moves = ["rock", "paper", "scissors"];
-    const randomIndex = Math.floor(Math.random() * moves.length);
-    return moves[randomIndex];
-  };
-  
-  const getOutcome = (moveOne, moveTwo) => {
-    if (moveOne === moveTwo) {
-      return "It's a draw!";
-    }
-  
-    if (
-      (moveOne === "scissors" && moveTwo === "paper") ||
-      (moveOne === "rock" && moveTwo === "scissors") ||
-      (moveOne === "paper" && moveTwo === "rock")
-    ) {
-      return "Player One wins!";
-    }
-  
-    return "Player Two wins!";
-  };
-  
-  // Removing elements (nodes) from the DOM
-  const resetGame = () => {
-    if (document.getElementById("outcome")) {
-      const outcome = document.body.lastChild;
-      document.body.removeChild(outcome);
-    }
-  };
-  
-  const playGame = () => {
-    resetGame();
-    const playerOneMove = getRandomMove();
-    const playerTwoMove = getRandomMove();
-    const outcome = getOutcome(playerOneMove, playerTwoMove);
-    updateDOM(playerOneMove, playerTwoMove, outcome);
-  };
-  
-  const updateDOM = (moveOne, moveTwo, outcome) => {
-    // TODO Implement this method to update the DOM
-    // There are some images you can use in the images directory
-  };
-  
-  const playButton = document.getElementById("play-btn");
-  playButton.addEventListener("click", playGame);
-  
+  const moves = ['rock', 'paper', 'scissors']
+  const randomIndex = Math.floor(Math.random() * moves.length)
+  return moves[randomIndex]
+}
+
+const getOutcome = (moveOne, moveTwo) => {
+  if (moveOne === moveTwo) {
+    return "It's a draw!"
+  }
+
+  if (
+    (moveOne === 'scissors' && moveTwo === 'paper') ||
+    (moveOne === 'rock' && moveTwo === 'scissors') ||
+    (moveOne === 'paper' && moveTwo === 'rock')
+  ) {
+    return 'Player One wins!'
+  }
+
+  return 'Player Two wins!'
+}
+
+// Removing elements (nodes) from the DOM
+const resetGame = () => {
+  if (document.getElementById('outcome')) {
+    const outcome = document.body.lastChild
+    document.body.removeChild(outcome)
+  }
+
+  //document.body.removeChild(rockImg)
+}
+
+const playGame = () => {
+  resetGame()
+  const playerOneMove = getRandomMove()
+  const playerTwoMove = getRandomMove()
+  const outcome = getOutcome(playerOneMove, playerTwoMove)
+  updateDOM(playerOneMove, playerTwoMove, outcome)
+}
+
+const updateDOM = (moveOne, moveTwo, outcome) => {
+  // TODO Implement this method to update the DOM
+  // There are some images you can use in the images directory
+  const rockImg = new Image(25, 25)
+
+  rockImg.src = '/images/rock.png'
+  const paperImg = new Image(25, 25)
+
+  paperImg.src = '/images/paper.png'
+  const scissorsImg = new Image(25, 25)
+
+  scissorsImg.src = '/images/scissors.png'
+
+  const card1 = document.getElementById('player-one')
+  const card2 = document.getElementById('player-two')
+
+  if (moveOne === 'rock') card1.append(rockImg)
+  else if (moveOne === 'paper') card1.append(paperImg)
+  else if (moveOne === 'scissors') card1.append(scissorsImg)
+
+  if (moveTwo === 'rock') card2.append(rockImg)
+  else if (moveTwo === 'paper') card2.append(paperImg)
+  else if (moveTwo === 'scissors') card2.append(scissorsImg)
+
+  document.body.append(outcome)
+}
+
+const playButton = document.getElementById('play-btn')
+playButton.addEventListener('click', playGame)
